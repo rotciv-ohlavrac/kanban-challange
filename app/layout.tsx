6d275bcd637cf5e6aff3881508cccb82c2a56247
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2025-09-10 21:24:54 +0000

    test: :test_tube: test if pr creation works

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+// just a comment to test pr creation
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
